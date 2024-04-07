a4d24802c11ed7e40d31f48864794005a1871c45
Palumbi <bpalumbi@UMICH.EDU>
2024-04-06 21:10:26 -0400

    Successfully loading multiple prefabs

--- Assets/SpawnObjs.cs
+++ Assets/SpawnObjs.cs
@@ -29,9 +29,9 @@ using UnityEngine.Serialization;
 public class SpawnObjs : MonoBehaviour
 {
     [SerializeField, Tooltip("Prefab to be placed into the scene, or object in the scene to be moved around.")]
-    public GameObject SpawnObject;
+    public List<GameObject> SpawnObjects;
     [SerializeField, Tooltip("Number of SpawnObject(s) to place into the scene, only applies to Prefabs.")]
-    public int SpawnAmount = 8;
+    public List<int> SpawnAmounts;
     [SerializeField, Tooltip("Maximum number of times to attempt spawning/moving an object before giving up.")]
     public int MaxIterations = 1000;
 
@@ -74,118 +74,124 @@ public class SpawnObjs : MonoBehaviour
     public void StartSpawn()
     {
         var room = MRUK.Instance.GetCurrentRoom();
-        UnityEngine.Debug.Log("hello");
-        var prefabBounds = Utilities.GetPrefabBounds(SpawnObject);
-        float minRadius = 0.0f;
-        const float clearanceDistance = 0.01f;
-        float baseOffset = -prefabBounds?.min.y ?? 0.0f;
-        float centerOffset = prefabBounds?.center.y ?? 0.0f;
-        Bounds adjustedBounds = new();
 
-        if (prefabBounds.HasValue)
-        {
-            minRadius = Mathf.Min(-prefabBounds.Value.min.x, -prefabBounds.Value.min.z, prefabBounds.Value.max.x, prefabBounds.Value.max.z);
-            if (minRadius < 0f)
-            {
-                minRadius = 0f;
-            }
-            var min = prefabBounds.Value.min;
-            var max = prefabBounds.Value.max;
-            min.y += clearanceDistance;
-            if (max.y < min.y)
-            {
-                max.y = min.y;
-            }
-            adjustedBounds.SetMinMax(min, max);
-            if (OverrideBounds > 0)
-            {
-                Vector3 center = new Vector3(0f, clearanceDistance, 0f);
-                Vector3 size = new Vector3(OverrideBounds * 2f, clearanceDistance * 2f, OverrideBounds * 2f); // OverrideBounds represents the extents, not the size
-                adjustedBounds = new Bounds(center, size);
-            }
-        }
+        for (int itemIndex = 0; itemIndex < SpawnObjects.Count; itemIndex++) {
 
-        for (int i = 0; i < SpawnAmount; ++i)
-        {
-            for (int j = 0; j < MaxIterations; ++j)
+            GameObject prefab = SpawnObjects[itemIndex];
+            int spawnCount = SpawnAmounts[itemIndex];
+
+            var prefabBounds = Utilities.GetPrefabBounds(prefab);
+            float minRadius = 0.0f;
+            const float clearanceDistance = 0.01f;
+            float baseOffset = -prefabBounds?.min.y ?? 0.0f;
+            float centerOffset = prefabBounds?.center.y ?? 0.0f;
+            Bounds adjustedBounds = new();
+
+            if (prefabBounds.HasValue)
             {
-                Vector3 spawnPosition = Vector3.zero;
-                Vector3 spawnNormal = Vector3.zero;
-                if (SpawnLocations == SpawnLocation.Floating)
+                minRadius = Mathf.Min(-prefabBounds.Value.min.x, -prefabBounds.Value.min.z, prefabBounds.Value.max.x, prefabBounds.Value.max.z);
+                if (minRadius < 0f)
                 {
-                    var randomPos = room.GenerateRandomPositionInRoom(minRadius, true);
-                    if (!randomPos.HasValue)
-                    {
-                        UnityEngine.Debug.Log("breaking early");
-                        break;
-                    }
-
-                    spawnPosition = randomPos.Value;
+                    minRadius = 0f;
                 }
-                else
+                var min = prefabBounds.Value.min;
+                var max = prefabBounds.Value.max;
+                min.y += clearanceDistance;
+                if (max.y < min.y)
                 {
-                    MRUK.SurfaceType surfaceType = 0;
-                    switch (SpawnLocations)
+                    max.y = min.y;
+                }
+                adjustedBounds.SetMinMax(min, max);
+                if (OverrideBounds > 0)
+                {
+                    Vector3 center = new Vector3(0f, clearanceDistance, 0f);
+                    Vector3 size = new Vector3(OverrideBounds * 2f, clearanceDistance * 2f, OverrideBounds * 2f); // OverrideBounds represents the extents, not the size
+                    adjustedBounds = new Bounds(center, size);
+                }
+            }
+
+            for (int i = 0; i < spawnCount; ++i)
+            {
+                for (int j = 0; j < MaxIterations; ++j)
+                {
+                    Vector3 spawnPosition = Vector3.zero;
+                    Vector3 spawnNormal = Vector3.zero;
+                    if (SpawnLocations == SpawnLocation.Floating)
                     {
-                        case SpawnLocation.AnySurface:
-                            surfaceType |= MRUK.SurfaceType.FACING_UP;
-                            surfaceType |= MRUK.SurfaceType.VERTICAL;
-                            surfaceType |= MRUK.SurfaceType.FACING_DOWN;
+                        var randomPos = room.GenerateRandomPositionInRoom(minRadius, true);
+                        if (!randomPos.HasValue)
+                        {
+                            UnityEngine.Debug.Log("breaking early");
                             break;
-                        case SpawnLocation.VerticalSurfaces:
-                            surfaceType |= MRUK.SurfaceType.VERTICAL;
-                            break;
-                        case SpawnLocation.OnTopOfSurfaces:
-                            surfaceType |= MRUK.SurfaceType.FACING_UP;
-                            break;
-                        case SpawnLocation.HangingDown:
-                            surfaceType |= MRUK.SurfaceType.FACING_DOWN;
-                            break;
+                        }
+
+                        spawnPosition = randomPos.Value;
                     }
-                    if (room.GenerateRandomPositionOnSurface(surfaceType, minRadius, LabelFilter.FromEnum(Labels), out var pos, out var normal))
+                    else
                     {
-                        spawnPosition = pos + normal * baseOffset;
-                        spawnNormal = normal;
-                        var center = spawnPosition + normal * centerOffset;
-                        // In some cases, surfaces may protrude through walls and end up outside the room
-                        // check to make sure the center of the prefab will spawn inside the room
-                        if (!room.IsPositionInRoom(center))
+                        MRUK.SurfaceType surfaceType = 0;
+                        switch (SpawnLocations)
+                        {
+                            case SpawnLocation.AnySurface:
+                                surfaceType |= MRUK.SurfaceType.FACING_UP;
+                                surfaceType |= MRUK.SurfaceType.VERTICAL;
+                                surfaceType |= MRUK.SurfaceType.FACING_DOWN;
+                                break;
+                            case SpawnLocation.VerticalSurfaces:
+                                surfaceType |= MRUK.SurfaceType.VERTICAL;
+                                break;
+                            case SpawnLocation.OnTopOfSurfaces:
+                                surfaceType |= MRUK.SurfaceType.FACING_UP;
+                                break;
+                            case SpawnLocation.HangingDown:
+                                surfaceType |= MRUK.SurfaceType.FACING_DOWN;
+                                break;
+                        }
+                        if (room.GenerateRandomPositionOnSurface(surfaceType, minRadius, LabelFilter.FromEnum(Labels), out var pos, out var normal))
+                        {
+                            spawnPosition = pos + normal * baseOffset;
+                            spawnNormal = normal;
+                            var center = spawnPosition + normal * centerOffset;
+                            // In some cases, surfaces may protrude through walls and end up outside the room
+                            // check to make sure the center of the prefab will spawn inside the room
+                            if (!room.IsPositionInRoom(center))
+                            {
+                                continue;
+                            }
+                            // Ensure the center of the prefab will not spawn inside a scene volume
+                            if (room.IsPositionInSceneVolume(center))
+                            {
+                                continue;
+                            }
+                            // Also make sure there is nothing close to the surface that would obstruct it
+                            if (room.Raycast(new Ray(pos, normal), SurfaceClearanceDistance, out _))
+                            {
+                                continue;
+                            }
+                        }
+                    }
+
+                    Quaternion spawnRotation = Quaternion.FromToRotation(Vector3.up, spawnNormal);
+                    if (CheckOverlaps && prefabBounds.HasValue)
+                    {
+                        if (Physics.CheckBox(spawnPosition + spawnRotation * adjustedBounds.center, adjustedBounds.extents, spawnRotation, LayerMask, QueryTriggerInteraction.Ignore))
                         {
                             continue;
                         }
-                        // Ensure the center of the prefab will not spawn inside a scene volume
-                        if (room.IsPositionInSceneVolume(center))
-                        {
-                            continue;
-                        }
-                        // Also make sure there is nothing close to the surface that would obstruct it
-                        if (room.Raycast(new Ray(pos, normal), SurfaceClearanceDistance, out _))
-                        {
-                            continue;
-                        }
                     }
-                }
 
-                Quaternion spawnRotation = Quaternion.FromToRotation(Vector3.up, spawnNormal);
-                if (CheckOverlaps && prefabBounds.HasValue)
-                {
-                    if (Physics.CheckBox(spawnPosition + spawnRotation * adjustedBounds.center, adjustedBounds.extents, spawnRotation, LayerMask, QueryTriggerInteraction.Ignore))
+                    if (prefab.gameObject.scene.path == null)
                     {
-                        continue;
+                        Instantiate(prefab, spawnPosition, spawnRotation, transform);
                     }
+                    else
+                    {
+                        prefab.transform.position = spawnPosition;
+                        prefab.transform.rotation = spawnRotation;
+                        return; // ignore SpawnAmount once we have a successful move of existing object in the scene
+                    }
+                    break;
                 }
-
-                if (SpawnObject.gameObject.scene.path == null)
-                {
-                    Instantiate(SpawnObject, spawnPosition, spawnRotation, transform);
-                }
-                else
-                {
-                    SpawnObject.transform.position = spawnPosition;
-                    SpawnObject.transform.rotation = spawnRotation;
-                    return; // ignore SpawnAmount once we have a successful move of existing object in the scene
-                }
-                break;
             }
         }
     }
